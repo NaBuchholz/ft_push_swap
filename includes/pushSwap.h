45b54f839f0563ad6a41115098b147c56ab1d597
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-04-10 16:13:22 -0300

    feat: add function prototypes for stack sorting and number validation

--- includes/pushSwap.h
+++ includes/pushSwap.h
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:34:05 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/04/10 14:53:25 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:45:56 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,12 @@ void	swap(t_stack *st_a, t_stack *st_b, char *op);
 void	push(t_stack *st_a, t_stack *st_b, char *op);
 void	rotate(t_stack *st_a, t_stack *st_b, char *op);
 void	rotate_re(t_stack *st_a, t_stack *st_b, char *op);
+void	sort_stacks(t_stack *stack_a, t_stack *stack_b);
 void	radix_sort(t_stack *stack_a, t_stack *stack_b);
 void	free_stack(t_stack *stack);
 void	exit_error(t_stack *stack_a, t_stack *stack_b);
 int		is_sorted(t_stack *stack);
 int		has_duplicate(t_stack *stack, int num);
+int		is_valid_number(char *str);
 
 #endif
