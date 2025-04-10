60837801dfa992a3a734a7cb7272dfd20e040bd4
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-04-10 01:24:18 -0300

    refactor: rename stack parameters for consistency in swap operations

--- includes/pushSwap.h
+++ includes/pushSwap.h
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:34:05 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/04/10 01:19:09 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/04/10 01:23:45 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ typedef struct s_stack
 
 int		validate(char **argv, t_list *inputList);
 long	ft_atol(char *number);
-void	swap(t_stack *stack_a, t_stack *stack_b, char *op);
-void	push(t_stack *stack_a, t_stack *stack_b, char *op);
-// void	ra(t_stack *stack_a, int print);
-// void	rb(t_stack *stack_b, int print);
-// void	rr(t_stack *stack_a, t_stack *stack_b, int print);
-// void	rra(t_stack *stack_a, int print);
-// void	rrb(t_stack *stack_b, int print);
-// void	rrr(t_stack *stack_a, t_stack *stack_b, int print);
+void	swap(t_stack *st_a, t_stack *st_b, char *op);
+void	push(t_stack *st_a, t_stack *st_b, char *op);
+// void	ra(t_stack *st_a, int print);
+// void	rb(t_stack *st_b, int print);
+// void	rr(t_stack *st_a, t_stack *st_b, int print);
+// void	rra(t_stack *st_a, int print);
+// void	rrb(t_stack *st_b, int print);
+// void	rrr(t_stack *st_a, t_stack *st_b, int print);
 
 #endif
--- src/swap_operations.c
+++ src/swap_operations.c
@@ -6,54 +6,54 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:01:45 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/04/10 01:19:09 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/04/10 01:23:45 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushSwap.h"
 
-static void	sa(t_stack *stack_a, int print)
+static void	sa(t_stack *st_a, int print)
 {
 	int	temp;
 
-	if (stack_a->size < 2)
+	if (st_a->size < 2)
 		return ;
-	temp = stack_a->val[0];
-	stack_a->val[0] = stack_a->val[1];
-	stack_a->val[1] = temp;
+	temp = st_a->val[0];
+	st_a->val[0] = st_a->val[1];
+	st_a->val[1] = temp;
 	if (print == 1)
 		ft_printf("sa\n");
 }
 
-static void	sb(t_stack *stack_b, int print)
+static void	sb(t_stack *st_b, int print)
 {
 	int	temp;
 
-	if (stack_b->size < 2)
+	if (st_b->size < 2)
 		return ;
-	temp = stack_b->val[0];
-	stack_b->val[0] = stack_b->val[1];
-	stack_b->val[1] = temp;
+	temp = st_b->val[0];
+	st_b->val[0] = st_b->val[1];
+	st_b->val[1] = temp;
 	if (print == 1)
 		ft_printf("sb\n");
 }
 
-static void	ss(t_stack *stack_a, t_stack *stack_b, int print)
+static void	ss(t_stack *st_a, t_stack *st_b, int print)
 {
-	if (!stack_a || !stack_b)
+	if (!st_a || !st_b)
 		return ;
-	sa(stack_a, 0);
-	sb(stack_b, 0);
+	sa(st_a, 0);
+	sb(st_b, 0);
 	if (print == 1)
 		ft_printf("ss\n");
 }
 
-void	swap(t_stack *stack_a, t_stack *stack_b, char *op)
+void	swap(t_stack *st_a, t_stack *st_b, char *op)
 {
-	if (stack_a && ft_strncmp(op, "sa", 3) == 0)
-		sa(stack_a, 1);
-	else if (stack_b && ft_strncmp(op, "sb", 3) == 0)
-		sb(stack_b, 1);
-	else if ((stack_a && stack_b) && ft_strncmp(op, "ss", 3) == 0)
-		ss(stack_a, stack_b, 1);
+	if (st_a && ft_strncmp(op, "sa", 3) == 0)
+		sa(st_a, 1);
+	else if (st_b && ft_strncmp(op, "sb", 3) == 0)
+		sb(st_b, 1);
+	else if ((st_a && st_b) && ft_strncmp(op, "ss", 3) == 0)
+		ss(st_a, st_b, 1);
 }
