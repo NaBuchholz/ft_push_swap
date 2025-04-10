a2f5a86a189870ca5128a61c5cea13073b9d9185
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-04-10 14:47:31 -0300

    feat: implement sorting functions for three and five element stacks

--- src/sort_stack.c
+++ src/sort_stack.c
@@ -6,11 +6,86 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:32:15 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/04/10 14:32:56 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/04/10 14:47:03 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushSwap.h"
+
+static void	sort_three(t_stack *stack_a)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = stack_a->val[0];
+	b = stack_a->val[1];
+	c = stack_a->val[2];
+	if (a > b && b < c && a < c)
+		swap(stack_a, NULL, "sa");
+	else if (a > b && b > c)
+	{
+		swap(stack_a, NULL, "sa");
+		rotate_re(stack_a, NULL, "rra");
+	}
+	else if (a > b && b < c && a > c)
+		rotate(stack_a, NULL, "ra");
+	else if (a < b && b > c && a < c)
+	{
+		swap(stack_a, NULL, "sa");
+		rotate(stack_a, NULL, "ra");
+	}
+	else if (a < b && b > c && a > c)
+		rotate_re(stack_a, NULL, "rra");
+}
+
+static int	find_min_position(t_stack *stack)
+{
+	int	min;
+	int	pos;
+	int	i;
+
+	min = INT_MAX;
+	pos = -1;
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->val[i] < min)
+		{
+			min = stack->val[i];
+			pos = i;
+		}
+		i++;
+	}
+	return (pos);
+}
+
+static void	sort_five(t_stack *stack_a, t_stack *stack_b)
+{
+	int	pos;
+	int	count;
+
+	count = 0;
+	while (count < 2)
+	{
+		pos = find_min_position(stack_a);
+		if (pos <= stack_a->size / 2)
+		{
+			while (pos-- > 0)
+				rotate(stack_a, NULL, "ra");
+		}
+		else
+		{
+			while (pos++ < stack_a->size)
+				rotate_re(stack_a, NULL, "rra");
+		}
+		push(stack_a, stack_b, "pb");
+		count++;
+	}
+	sort_three(stack_a);
+	while (stack_b->size > 0)
+		push(stack_a, stack_b, "pa");
+}
 
 void	sort_stacks(t_stack *stack_a, t_stack *stack_b)
 {
